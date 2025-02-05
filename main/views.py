e6e2d5d658ee4db3d07ca7aae9a9987ca3b48b75
INSANITY6406 <skshaikhsahal064@gmail.com>
2025-02-06 00:09:36 +0530

    Added Functions to Read PDFs

--- main/views.py
+++ main/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+import pytesseract
+from pdf2image import convert_from_path
 
 # Create your views here.
+
+
+def extract_text(pdf_path):
+    images=convert_from_path(pdf_path)
+    text="\n".join([pytesseract.image_to_string(image) for image in images])
+    return text
+
+print(extract_text("Scanned Page.pdf"))
