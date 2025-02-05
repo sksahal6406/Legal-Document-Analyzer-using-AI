e085f2bd4ee98241275ae6d109354fc9ce1422a7
INSANITY6406 <skshaikhsahal064@gmail.com>
2025-02-06 01:23:27 +0530

    Improved OCR

--- main/views.py
+++ main/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 
+import pytesseract
+from pdf2image import convert_from_path
+
+#for Image Proccessing
+import cv2
+import numpy as np
+from PIL import Image
 
 # for language translation and speech translation
 from googletrans import Translator
@@ -22,10 +29,31 @@ def translate_text(text,language):
     return result.text
 
 
+def extract_text(pdf_path):
+    images=convert_from_path(pdf_path)
+    for i,img in enumerate(images):
+        img_path=f"E:\DJango Projects\LegalAI\images\img_{i+1}.png"
+        img.save(img_path,"PNG")
+        print(f"Extracted: {img_path}")
+    # for image in images:
+        img=cv2.imread(img_path)
+        gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        # cv2.imwrite(f"test_{i+1}.png",gray)
+        contrast=cv2.createCLAHE(clipLimit=2.0,tileGridSize=(8,8))
+        contrast_img=contrast.apply(gray)
+        
+        blurred=cv2.GaussianBlur(contrast_img,(5,5),0)
+
+        _,binary=cv2.threshold(blurred,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+        cv2.imwrite(f"finalimg_{i+1}.png",binary)
+        text="\n".join([pytesseract.image_to_string(binary,config="--psm 6 --oem 3")])
+        print(text)
+    
+
+
+print(extract_text("E:\DJango Projects\LegalAI\\test1.pdf"))
 
 ################################################## views ###################################################
-import pytesseract
-from pdf2image import convert_from_path
 
 # Create your views here.
 def index(request):
@@ -38,10 +66,3 @@ def analyze(request):
 
     return render(request, 'analyze.html', {'extracted_text':extract_text,'translated_text':translated_text})
     
-
-def extract_text(pdf_path):
-    images=convert_from_path(pdf_path)
-    text="\n".join([pytesseract.image_to_string(image) for image in images])
-    return text
-
-print(extract_text("Scanned Page.pdf"))
