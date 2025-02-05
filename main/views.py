8de4cc0c8d48ea507b7255c1f76d5bf1f553d792
Shubham Patel <sanket13052004@gmail.com>
2025-02-06 00:05:55 +0530

    text to speech and translation fucntion done

--- main/views.py
+++ main/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse
+
+
+# for language translation and speech translation
+from googletrans import Translator
+from gtts import gTTS
+
+################################################ functions ################################################
+
+def text_to_speech(text):
+    tts = gTTS(text=text, lang='ta',slow=False)
+    tts.save(f'{text}.mp3')
+
+
+
+def translate_text(text,language):
+    translator = Translator()
+    result = translator.translate(text, dest=language)
+    text_to_speech(result.text)
+    print(result.text)
+    return result.text
+
+
+
+################################################## views ###################################################
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def analyze(request):
+    extract_text = "my name is sanket"
+    translated_text = "hello world"
+    translate_text(extract_text,'ta')
+
+    return render(request, 'analyze.html', {'extracted_text':extract_text,'translated_text':translated_text})
+    
